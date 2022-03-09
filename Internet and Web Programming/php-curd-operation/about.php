6e6ecc52a75d958df57d9de45772bd83aacfc06d
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2022-03-09 10:46:47 +0600

    IWP Lab Exam

---
+++ Internet and Web Programming/php-curd-operation/about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+      <!-- Our team  section -->
+      <div>
+        <h1 style="text-align: center; margin-top: 2%"> Our team</h1>
+        <div class="grid-container">
+            <div class="team">
+                <img src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/86471431_2713804752210763_7793095511505895424_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHDbNg7lld3eUQsFx3rZBS1Di6GBKC0O04OLoYEoLQ7Ti2TKBDFsdXs8_vWlvHEQODhpFI7btCJZe0RgWMwYVom&_nc_ohc=NEXYsywB1gkAX88ibAx&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-a-5ScRdNCw5U20KUnKGgK0yn-QCxrniXmhzU9z3p49A&oe=623F9B61"
+                    alt="noman" style="width: 100%;">
+                <div class="container">
+                    <h3><b>Noman</b></h3>
+                    <p>Department : ICE</p>
+                    <p>Roll : ASH1811048M</p>
+                </div>
+
+            </div>
+            <div class="team">
+                <img src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/86471431_2713804752210763_7793095511505895424_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHDbNg7lld3eUQsFx3rZBS1Di6GBKC0O04OLoYEoLQ7Ti2TKBDFsdXs8_vWlvHEQODhpFI7btCJZe0RgWMwYVom&_nc_ohc=NEXYsywB1gkAX88ibAx&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-a-5ScRdNCw5U20KUnKGgK0yn-QCxrniXmhzU9z3p49A&oe=623F9B61"
+                    alt="noman" style="width: 100%;">
+                <div class="container">
+                    <h3><b>Noman</b></h3>
+                    <p>Department : ICE</p>
+                    <p>Roll : ASH1811048M</p>
+                </div>
+
+            </div>
+            <div class="team">
+                <img src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/86471431_2713804752210763_7793095511505895424_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHDbNg7lld3eUQsFx3rZBS1Di6GBKC0O04OLoYEoLQ7Ti2TKBDFsdXs8_vWlvHEQODhpFI7btCJZe0RgWMwYVom&_nc_ohc=NEXYsywB1gkAX88ibAx&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-a-5ScRdNCw5U20KUnKGgK0yn-QCxrniXmhzU9z3p49A&oe=623F9B61"
+                    alt="noman" style="width: 100%;">
+                <div class="container">
+                    <h3><b>Noman</b></h3>
+                    <p>Department : ICE</p>
+                    <p>Roll : ASH1811048M</p>
+                </div>
+
+            </div>
+            <div class="team">
+                <img src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/86471431_2713804752210763_7793095511505895424_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHDbNg7lld3eUQsFx3rZBS1Di6GBKC0O04OLoYEoLQ7Ti2TKBDFsdXs8_vWlvHEQODhpFI7btCJZe0RgWMwYVom&_nc_ohc=NEXYsywB1gkAX88ibAx&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-a-5ScRdNCw5U20KUnKGgK0yn-QCxrniXmhzU9z3p49A&oe=623F9B61"
+                    alt="noman" style="width: 100%;">
+                <div class="container">
+                    <h3><b>Noman</b></h3>
+                    <p>Department : ICE</p>
+                    <p>Roll : ASH1811048M</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
